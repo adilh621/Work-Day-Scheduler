f5f413681d7d1ab6d040f029309a9c573f065e99
Adil Hussain <adilh621@gmail.com>
2020-09-09 18:23:07 -0400

    finished web app , still waiting to see if bugs occur within the moment.js library in the color conditions

--- Develop/script.js
+++ Develop/script.js
@@ -1,51 +1,70 @@
 $(document).ready(function () {
 
-// prints current date and time to user
-var current = moment().format('MMMM Do YYYY, h:mm:ss a');;
-$("#currentDay").text("Today is the " + current);
+    // prints current date and time to user
+    var current = moment().format('MMMM Do YYYY, h:mm:ss a');;
+    $("#currentDay").text("Today is  " + current);
 
 
 
 
-// dynamically print sections for timeblocks
-for(var i = 0 ; i <10 ; i++){
-    var row = $('<div>').addClass('row');
-    var time = $('<div>').addClass('hour col-md-2').text(moment('9:00am', 'hh:mm A').add(i, 'hours').format('hA'));
-    time.attr('data-time', moment('9:00am', 'hh:mm a').add(i, 'hours').format('hA'));
-
+    // dynamically print sections for timeblocks
+    for(var i = 0 ; i <10 ; i++){
+        var row = $('<div>').addClass('row');
+        var time = $('<div>').addClass('hour col-md-2').text(moment('9:00am', 'hh:mm A').add(i, 'hours').format('hA'));
+        time.attr('data-time', moment('9:00am', 'hh:mm a').add(i, 'hours').format('hA'));
+        time.attr('value' , parseInt(moment('9:00am', 'hh:mm a').add(i, 'hours').format('hA')));
     
-    // data input section
-    var textData = $('<textarea>').addClass('col-md-9');
     
-    var saveButton = $('<button>').addClass('saveBtn col-md-1').html('save');
-
-    $('.container').append(row);
-
-    //time of day section (far left)
-    $(row).append(time);
-
-    //user text space (middle)
-    $(time).after(textData);
-
-    //save button (far right)
-    $(textData).after(saveButton);
-}
-
-
-// Retrieves data saved from local storage upon loading
-$(document).ready(function(){
-        
-    hours = $('.hour').toArray();
-    for (var i = 0 ; i < hours.length ; i++) {
+        var textData = $('<textarea>').addClass('col-md-9');
     
-        
-    $(hours[i]).siblings('textarea').text(localStorage.getItem($(hours[i]).attr('data-time')));
+        var saveButton = $('<button>').addClass('saveBtn col-md-1').html('save');
+
+        $('.container').append(row);
+
+        //time of day section (far left)
+        $(row).append(time);
+
+        //user text space (middle)
+        $(time).after(textData);
+
+        //save button (far right)
+        $(textData).after(saveButton);
     }
+
+
+    // Retrieves data saved from local storage upon loading
+    $(document).ready(function(){
+        
+        hours = $('.hour').toArray();
+        for (var i = 0 ; i < hours.length ; i++) {
+    
+        
+        $(hours[i]).siblings('textarea').text(localStorage.getItem($(hours[i]).attr('data-time')));
+        }
+        })
+
+    // Function to save data after save button is pressed
+    $(".saveBtn").on("click" , function(){localStorage.setItem($(this).siblings('div.hour').attr('data-time'), $(this).siblings('textarea').val())})
+
+    // Function to determine color of blocks
+    $(document).ready(function(){
+        var now = parseInt(moment().format('h:mm:ss a'));
+
+        
+        console.log(now , parseInt(time.attr('value')));
+        
+        if(now.toString() === time.attr('value') ){
+            $(textData).addClass('present');
+            console.log("i was call");
+        }
+        else if(now() > parseInt(time.attr('value'))){
+            $(textData).addClass('past');
+            console.log("i was call");
+        }
+        else{
+            $(textData).addClass('future');
+        }
     })
-
-// Function to save data after save button is pressed
-$(".saveBtn").on("click" , function(){localStorage.setItem($(this).siblings('div.hour').attr('data-time'), $(this).siblings('textarea').val())})
-
 
 })
 
--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
 You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+
+## screenshots of deployed page
+![image](https://user-images.githubusercontent.com/36384770/92660866-46e54400-f2c9-11ea-97a1-a144d875426a.png)
