232492836b15038d81fa40dffa585ca78855a65b
Adil Hussain <adilh621@gmail.com>
2020-09-09 17:31:07 -0400

    finished logic for calender functionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
+
 // prints current date and time to user
 var current = moment().format('MMMM Do YYYY, h:mm:ss a');;
 $("#currentDay").text("Today is the " + current);
 
-// dynamically print sections for timeblocks
 
+
+
+// dynamically print sections for timeblocks
 for(var i = 0 ; i <10 ; i++){
     var row = $('<div>').addClass('row');
     var time = $('<div>').addClass('hour col-md-2').text(moment('9:00am', 'hh:mm A').add(i, 'hours').format('hA'));
@@ -13,41 +16,42 @@ for(var i = 0 ; i <10 ; i++){
     
     // data input section
     var textData = $('<textarea>').addClass('col-md-9');
-
-    // save button
+    
     var saveButton = $('<button>').addClass('saveBtn col-md-1').html('save');
 
     $('.container').append(row);
 
-    // time block in row
+    //time of day section (far left)
     $(row).append(time);
 
-    // then data input section
+    //user text space (middle)
     $(time).after(textData);
 
-    // then save button to submit to local storage 
+    //save button (far right)
     $(textData).after(saveButton);
-
 }
 
 
-    var currentTime = parseInt(moment().format('h:mm:ss a'));
-    localStorage.setItem($(this).siblings('div.hour').attr('data-time'), $(this).siblings('textarea').val())
+// Retrieves data saved from local storage upon loading
+$(document).ready(function(){
+        
+    hours = $('.hour').toArray();
+    for (var i = 0 ; i < hours.length ; i++) {
     
-    hourArr = $('.hour').toArray();
-    console.log(hourArr);
-    for (var i of hourArr) {
-    $(hourArr[i]).siblings('textarea').text(localStorage.getItem($(hourArr[i]).attr('data-time')));
-
-    
+        
+    $(hours[i]).siblings('textarea').text(localStorage.getItem($(hours[i]).attr('data-time')));
     }
-    console.log(localStorage.getItem($(hourArr[i]).attr('data-time')));
+    })
 
-
-
+// Function to save data after save button is pressed
+$(".saveBtn").on("click" , function(){localStorage.setItem($(this).siblings('div.hour').attr('data-time'), $(this).siblings('textarea').val())})
 
 
 })
+
+
+
+
 
 
 
